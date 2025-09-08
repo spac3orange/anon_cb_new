b69437b08d145cd2bed9dfbad3f5ac7ae046156f
spac3orange <m4gi3k44@proton.me>
2025-09-08 16:18:45 +0300

    cm3

--- app2/keyboards/pop_up_menu.py
+++ app2/keyboards/pop_up_menu.py
@@ -3,10 +3,12 @@ from aiogram.types import BotCommand
 
 async def set_commands_menu(bot):
     main_menu_commands = [
+        BotCommand(command='/start',
+                   description='📟 Главное меню'),
         BotCommand(command='/search',
-                   description='Поиск собеседника'),
+                   description='🔎 Поиск собеседника'),
         BotCommand(command='/stop',
-                   description='Остановить диалог')
+                   description='❌ Остановить диалог')
         ]
 
     await bot.set_my_commands(main_menu_commands)
